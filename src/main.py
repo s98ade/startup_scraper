3437a01624dc9e1b3a4a197a51ed4c4aedd6f68c
sade <sofia.ade@gmx.de>
2024-09-24 15:41:27 +0300

    simple scraper, saving in csv-file

--- src/main.py
+++ src/main.py
@@ -6,10 +6,19 @@ url = requests.get('https://startup100.net/').text
 
 soup = BeautifulSoup(url, 'html.parser')
 
-table = soup.find('table')
+table = soup.find('tbody')
 
-tbody = table.tbody
+with open('../data/startup_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    
+    for row in table.find_all('tr'):
+        row_data = []
+        
+        for cell in row.find_all('td'):
+            cell_text = cell.get_text(strip=True) #strip off extra space from text
+            
+            row_data.append(cell_text)
+            
+        csvwriter.writerow(row_data)
 
-print(tbody.prettify())
-# print(table.prettify())
-# print(soup.prettify())
+print("Data retrieved...Information saved in ../data/startup_data.csv")
